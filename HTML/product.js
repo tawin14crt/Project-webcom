f9139cd4880d5c3b720865559dfd34f9cc1fde0d
Tawin <tawin14crt@gmail.com>
2026-08-22 22:09:47 +0700

    patch2

---
+++ HTML/product.js
@@ -0,0 +1,16 @@
+const products = {
+            arabica: {
+                productname: "Arabica Coffee Beans",
+                productdescription: "รสชาติซับซ้อน กรดนุ่ม",
+                productimg: "../image/Product/arabicaSeeds.webp",
+                productprice: 250,
+                productstock: 6
+            },
+            robusta: {
+                productname: "Robusta Green Coffee Beans",
+                productdescription: "รสชาติซับซ้อน กรดนุ่ม",
+                productimg: "../image/Product/robusta-green-coffee-beans-500x500.webp",
+                productprice: 220,
+                productstock: 10
+            }
+        };
